737569d6cdc5cb11982931969fd0485d0fd28b0b
Joshua Shen <59071877+josh-shen@users.noreply.github.com>
2022-06-01 23:09:37 -0500

    UDP and TCP connections to server

---
+++ client_functions.py
@@ -0,0 +1,85 @@
+import bcrypt, hashlib, pickle, socket, time, threading
+
+from aes import aes_cipher, create_machine
+import utils
+import client_functions as cl
+
+# CONNECTION VARIABLES
+connect_type = 0
+reply = None
+lock = threading.Lock()
+
+# client credentials
+ID = "MqoZnhsHI3ZYz3ea6K4q"
+password = "password"
+
+# message receive thread
+def msg_recv(machine: aes_cipher):
+    while True:
+        encrypted_bytes = client_socket.tcp_client.recv(65536)
+        decrypted_bytes = machine.decrypt_message(encrypted_bytes)
+        message = pickle.loads(decrypted_bytes)
+        
+        if message["message_type"] == "CONNECTED":
+            print("connected to server")
+
+client_socket = cl.client_API(ID, password)
+
+while True:
+    if connect_type == 0:
+        print("type 'logon' to start connection  or 'exit' to shut the app")
+        ins = input()
+        print(ins)
+        if ins == "logon":
+            connect_type = 1
+        elif ins == "exit":
+            utils.screenClear()
+            exit()
+        else:
+            print("invalid input")
+    elif connect_type == 1:
+        try:
+            if reply == None:
+                client_socket.HELLO() 
+            elif reply != [] and reply[0] == "CHALLENGE":
+                salt = reply[1]
+                client_socket.RESPONSE(password, salt.encode())
+            elif reply != [] and reply[0] == "AUTH_SUCCESS":
+                connect_type = 2
+                # begin TCP connection
+                print(PORT, HOST)
+                client_socket.tcp_client.connect((HOST, int(PORT)))
+                message = utils.messageDict(ID, "CONNECT", cookie = cookie)
+                client_socket.CONNECT(message, machine)
+                print("sent tcp connect message")
+                recv_thread = threading.Thread(target = msg_recv, args = (machine,))
+                recv_thread.start()
+            elif reply != [] and reply[0] == "AUTH_FAIL":
+                reply = None
+                client_socket.udp_client.close()
+                print("authentication failed")
+                break
+
+            if connect_type == 1:
+                client_socket.udp_client.settimeout(5)
+                reply = client_socket.udp_client.recv(1024)
+                # authentication
+                bytes_check = reply[:2]
+                if bytes_check == b'as':
+                    machine = create_machine(password, client_socket.salt)
+                    reply = machine.decrypt_message(reply[2:])
+                    reply = reply.decode('utf-8').split()
+                    PORT = reply[1]
+                    HOST = reply[2]
+                    cookie = reply[3]
+                else:
+                    reply = reply.decode('utf-8').split() 
+                print("received from server: ", reply)
+        except socket.timeout:
+            reply = None
+            print("time out")
+            break
+    else:
+        message_input = input("> ")
+
+client_socket.tcp_client.close()
