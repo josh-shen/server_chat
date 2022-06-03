82da42f9df639b810cf475cd47f5c689ee4306cc
Joshua Shen <59071877+josh-shen@users.noreply.github.com>
2022-06-02 23:15:49 -0500

    Add username to message dict

--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
 import os
 from uuid import uuid4
 
-def messageDict(senderID, message_type, message_body = None, targetID = None, cookie = None, hashed_password = None, salt = None):
+def messageDict(senderID, message_type, username = None, message_body = None, targetID = None, cookie = None, hashed_password = None, salt = None, port = None):
     return { 
     'senderID' : senderID, 
     'message_type' : message_type,
+    'username': username,
     'message_body' : message_body,
     'targetID' : targetID,
     'cookie': cookie,
+    'port': port
     }
 
 def screenClear():
